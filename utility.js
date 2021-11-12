982f06ecd5a80634c5394913f6515935b2361086
TheButtskie <65842772+TheButtskie@users.noreply.github.com>
2021-11-11 23:38:17 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -14,11 +14,11 @@ var getJSON = function(url, callback) {
 };
 And use it like this:
 
-getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback',
+getJSON('https://fastapi-btest.herokuapp.com/',
 function(err, data) {
   if (err !== null) {
     alert('Something went wrong: ' + err);
   } else {
-    alert('Your query count: ' + data.query.count);
+    alert('Your query count: ' + data);
   }
 });
